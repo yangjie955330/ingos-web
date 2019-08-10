09df6bc3166f99961c2503499b5417aea8a74f8a
Danvic Wang <danvic96@hotmail.com>
2019-08-10 17:31:32 +0800

    Config webpack

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,40 @@
 // webpack config file
+
+// import path module and make file path to absolute path
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+// config options
+module.exports = {
+  /*
+    * all configuration options you can see in this site => https://cli.vuejs.org/config/
+    */
+
+  // the base url address when publish you site
+  publicPath: '/',
+  // the output dir when build this project
+  outputDir: 'dist',
+  // a dir address which generate static files(js,css,imgs,fonts,etc)
+  assetsDir: 'assets',
+  // whether to show lint error info in terminal
+  lintOnSave: process.env.NODE_ENV === 'development',
+  // show lint error and warning info
+  devServer: {
+    overlay: {
+      warning: true,
+      error: true,
+    },
+  },
+  // config webpack
+  configureWebpack: {
+    // set absolute path
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+  },
+};
