a65c116870b147d2cffd4728c8cb89a385f5f189
Danvic Wang <danvic96@hotmail.com>
2019-08-10 23:06:02 +0800

    Deploy the exampe project in github page

---
+++ dist/assets/js/chunk-2d0c4d9c.1b4ceddb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4d9c"],{"3d1f":function(n,c,u){"use strict";u.r(c);var l,d,e=u("2877"),o={},p=Object(e["a"])(o,l,d,!1,null,null,null);c["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0c4d9c.1b4ceddb.js.map
--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
    */
 
   // the base url address when publish you site, the example site will deploy it in github page.
-  // when deploy it into you own server, you can change the site pre address to '/' 
+  // when deploy it into you own server, you can change the site pre address to '/'
   publicPath: '/ingos-web/',
   // the output dir when build this project
   outputDir: 'dist',
