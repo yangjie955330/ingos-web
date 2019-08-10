be6e33c54a2ccd890ace974e1b790ce48e631dcf
Danvic Wang <danvic96@hotmail.com>
2019-08-10 23:35:34 +0800

    Add deploy bash script and delete dist files

--- dist/assets/js/chunk-2d0e95df.442dedca.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(n,u,c){"use strict";c.r(u);var e,l,d=c("2877"),o={},p=Object(d["a"])(o,e,l,!1,null,null,null);u["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d0e95df.442dedca.js.map
