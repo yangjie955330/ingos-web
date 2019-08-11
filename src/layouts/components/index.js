562bf3bdf163613a19a8eece0a6377165012d1cf
Danvic Wang <danvic96@hotmail.com>
2019-08-11 11:33:54 +0800

    Add layout page

---
+++ src/layouts/components/index.js
@@ -0,0 +1,4 @@
+// export site layout components
+export { default as navbar } from './navbar.vue';
+export { default as sidebar } from './sidebar/index.vue';
+export { default as app } from './app.vue';
---
+++ src/layouts/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
+      </div>
+      <app />
+    </div>
+  </div>
+</template>
+
+<script>
+// import navbar component
+import {
+  navbar,
+  sidebar,
+  app,
+} from './components';
+
+export default {
+  name: 'layout',
+  components: {
+    navbar,
+    sidebar,
+    app,
+  },
+  computed: {
+    sidebar() {
+      // return this.$store.state.app.sidebar
+      return {
+        opened: true,
+        withoutAnimation: true,
+      };
+    },
+    device() {
+      // return this.$store.state.app.device
+      return 'pc';
+    },
+    fixedHeader() {
+      // return this.$store.state.settings.fixedHeader
+      return true;
+    },
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile',
+      };
+    },
+  },
+};
+
+</script>
